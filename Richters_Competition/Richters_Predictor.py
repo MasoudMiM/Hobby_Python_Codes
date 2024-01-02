3fa59c9a351a40c10226a3526c65e11c59a2983b
Masoud Masoumi <masoumi.masoud@gmail.com>
2024-01-01 21:01:44 -0500

    cleaning up the repo from irrelavant files.

---
+++ Richters_Competition/Richters_Predictor.py
@@ -0,0 +1,9 @@
+# This is a code written to pariticpate in the Richters Predictor: Modeling Earthquake Damage competition on 
+# DrivenData (https://www.drivendata.org/competitions/57/nepal-earthquake/).
+
+# based on the blog posted by the competition: (see https://drivendata.co/blog/richters-predictor-benchmark)
+# goal is to predict the level of damage a building suffered as a result of the 2015 earthquake. 
+# The data comes from the 2015 Nepal Earthquake Open Data Portal, and mainly consists of information on the buildings' 
+# structure and their legal ownership. Each row in the dataset represents a specific building in the 
+# region that was hit by Gorkha earthquake.
+
