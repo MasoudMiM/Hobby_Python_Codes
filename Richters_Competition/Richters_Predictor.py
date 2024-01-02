6be13f0799da3b9f3a341562276e4413fde0a51a
Masoud Masoumi <masoumi.masoud@gmail.com>
2024-01-01 21:26:01 -0500

    fixed one line of comment.

--- Richters_Competition/Richters_Predictor.py
+++ Richters_Competition/Richters_Predictor.py
@@ -4,8 +4,8 @@
 # based on the blog posted by the competition: (see https://drivendata.co/blog/richters-predictor-benchmark)
 # goal is to predict the level of damage a building suffered as a result of the 2015 earthquake. 
 # The data comes from the 2015 Nepal Earthquake Open Data Portal, and mainly consists of information on the buildings' 
-# structure and their legal ownership. Each row in the dataset represents a specific building in the 
-# region that was hit by Gorkha earthquake.
+# structure and their legal ownership. Each row in the dataset represents a specific 
+# building in the region that was hit by Gorkha earthquake.
 
 ######### Importing the libraries
 import numpy as np
