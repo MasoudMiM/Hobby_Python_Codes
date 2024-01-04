ccae277b785b022d775daae8f0721d79cf920fd7
Masoud Masoumi <masoumi.masoud@gmail.com>
2024-01-04 09:47:57 -0500

    tests some simple logistic regression for baseline results

--- Richters_Competition/Richters_Predictor.py
+++ Richters_Competition/Richters_Predictor.py
@@ -19,6 +19,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.metrics import f1_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
 ############# Looking into the features, initial exploratory data analysis, and some initial preparation
 df_train_values = pd.read_csv(f"{directory}train_values.csv")
 df_train_labels = pd.read_csv(f"{directory}train_labels.csv")
@@ -52,4 +58,32 @@ plt.savefig(f"{directory}correlation_matrix.png")
 #sns.pairplot(df_train_total, hue="damage_grade")
 #plt.savefig("Richters_Competition/pairplot.png")
 
+
+
+############# Initial attempt - creating a logistic regression model using all features
+X_train, X_val, y_train, y_val = train_test_split(df_train_values, df_train_labels['damage_grade'], train_size=0.8)
+
+# data normalization
+min_max_scaler = MinMaxScaler()
+x_train_scaled = min_max_scaler.fit_transform(X_train)
+x_val_scaled = min_max_scaler.fit_transform(X_val)
+
+# developing the logistic regression model - no hyperparameter tuning
+logreg = LogisticRegression()
+logreg.fit(x_train_scaled, y_train) 
+y_pred = logreg.predict(x_val_scaled)
+print(f"The f1 score for the logistic regression model is {f1_score(y_val, y_pred, average='micro'):.3f}")
+
+# Now, let's do a grid search and see if we can improve the model
+# defining the parameter range
+param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100],  
+              'penalty': ['l1', 'l2']}
+grid = GridSearchCV(LogisticRegression(), param_grid, refit = True, verbose = 1)
+# developing the model using the grid search best parameters
+grid.fit(x_train_scaled, y_train)
+print(f"The best parameters are {grid.best_params_}")
+print(f"The best score is {grid.best_score_:.3f}")
+y_pred = grid.predict(x_val_scaled)
+print(f"The f1 score for the logistic regression model is {f1_score(y_val, y_pred, average='micro'):.3f}")
+# NOTE: Even with the grid search, the f1 score is not improved.
 
