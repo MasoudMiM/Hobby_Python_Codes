f8783434a06ec19763c5a2033d3802b9a7ab7133
Masoud Masoumi <masoumi.masoud@gmail.com>
2024-01-05 22:35:24 -0500

    added function with five ML models
    The code now includes a function that takes in a model, and the data
    and it normalizes the data. It then perofrms hyper-param. tunning
    for each of the five types of input models, i.e. logistic regression,
    random forest, gradient boosting, svc, and knn.
    
    It then uses the function in a for-loop structure to run each model
    and finally uses the best selected model to fir the test data.

--- Richters_Competition/Richters_Predictor.py
+++ Richters_Competition/Richters_Predictor.py
@@ -20,10 +20,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.metrics import f1_score
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 
 ############# Looking into the features, initial exploratory data analysis, and some initial preparation
 df_train_values = pd.read_csv(f"{directory}train_values.csv")
@@ -58,32 +62,81 @@ plt.savefig(f"{directory}correlation_matrix.png")
 #sns.pairplot(df_train_total, hue="damage_grade")
 #plt.savefig("Richters_Competition/pairplot.png")
 
-
-
-############# Initial attempt - creating a logistic regression model using all features
+############### Let's define a function that takes in a model and returns the f1 score
 X_train, X_val, y_train, y_val = train_test_split(df_train_values, df_train_labels['damage_grade'], train_size=0.8)
-
-# data normalization
-min_max_scaler = MinMaxScaler()
-x_train_scaled = min_max_scaler.fit_transform(X_train)
-x_val_scaled = min_max_scaler.fit_transform(X_val)
-
-# developing the logistic regression model - no hyperparameter tuning
-logreg = LogisticRegression()
-logreg.fit(x_train_scaled, y_train) 
-y_pred = logreg.predict(x_val_scaled)
-print(f"The f1 score for the logistic regression model is {f1_score(y_val, y_pred, average='micro'):.3f}")
-
-# Now, let's do a grid search and see if we can improve the model
-# defining the parameter range
-param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100],  
+# the function will include the data normalization and the grid search
+# the function will also print the best parameters and the best score
+# the function will also print the f1 score for the validation set
+# the function should also return the model so it can be applied to the test data later on
+def model_f1_score(model, X_train, y_train, X_val, y_val):
+    # data normalization
+    min_max_scaler = MinMaxScaler()
+    x_train_scaled = min_max_scaler.fit_transform(X_train)
+    x_val_scaled = min_max_scaler.fit_transform(X_val)
+    # defining the parameter range for five different models, including 
+    # logistic regression, random forest, gradient boosting, support vector machine, and k-nearest neighbors
+    n_sampled_parameters = 5
+    if model == logreg:
+        param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100],  
               'penalty': ['l1', 'l2']}
-grid = GridSearchCV(LogisticRegression(), param_grid, refit = True, verbose = 1)
-# developing the model using the grid search best parameters
-grid.fit(x_train_scaled, y_train)
-print(f"The best parameters are {grid.best_params_}")
-print(f"The best score is {grid.best_score_:.3f}")
-y_pred = grid.predict(x_val_scaled)
-print(f"The f1 score for the logistic regression model is {f1_score(y_val, y_pred, average='micro'):.3f}")
-# NOTE: Even with the grid search, the f1 score is not improved.
+        grid = RandomizedSearchCV(model, param_grid, n_iter=5, refit = True, n_jobs=n_sampled_parameters, verbose = 1)
+    elif model == rf:
+        param_grid = {'n_estimators': [100, 200, 300, 400, 500],  
+              'max_depth': [5, 10, 15, 20, 25]}
+        grid = RandomizedSearchCV(model, param_grid, n_iter=5, refit = True, n_jobs=n_sampled_parameters, verbose = 1)
+    elif model == gb:
+        param_grid = {'n_estimators': [100, 200, 300, 400, 500],  
+              'max_depth': [5, 10, 15, 20, 25]}
+        grid = RandomizedSearchCV(model, param_grid,  n_iter=5, refit = True, n_jobs=n_sampled_parameters, verbose = 1)
+    elif model == svc:
+        param_grid = {'C': [0.1, 1, 10, 100],  
+              'gamma': [1, 0.1, 0.01, 0.001],
+              'kernel': ['rbf', 'poly', 'sigmoid']}
+        grid = RandomizedSearchCV(model, param_grid, n_iter=5, refit = True, n_jobs=n_sampled_parameters, verbose = 1)
+    elif model == knn:
+        param_grid = {'n_neighbors': [3, 5, 7, 9, 11],  
+              'weights': ['uniform', 'distance'],
+              'metric': ['euclidean', 'manhattan']}
+        grid = RandomizedSearchCV(model, param_grid, n_iter=5, refit = True, n_jobs=n_sampled_parameters, verbose = 1)
+    else:
+        print("The model is not defined!")
+
+    # developing the model using the grid search best parameters
+    grid.fit(x_train_scaled, y_train)
+    print(f"The best parameters are {grid.best_params_}")
+    print(f"The best score is {grid.best_score_:.3f}")
+    # predict the validation set
+    y_pred = grid.predict(x_val_scaled)
+    print(f"The f1 score for the logistic regression model is {f1_score(y_val, y_pred, average='micro'):.3f}")
+    return grid
+
+# let's define try five different models and see which one performs the best
+# includeing logistic regression, random forest, gradient boosting, support vector machine, and k-nearest neighbors
+
+logreg = LogisticRegression(max_iter=500)
+rf = RandomForestClassifier()
+gb = GradientBoostingClassifier()
+svc = SVC()
+knn = KNeighborsClassifier()
+
+model_dic = {}
+models = [logreg, rf, gb, svc, knn]
+model_names = ['logreg', 'rf', 'gb', 'svc', 'knn']
+for model, model_name in zip(models, model_names):
+    print(f"Model: {model_name}")
+    model_dic[model_name]=model_f1_score(model, X_train, y_train, X_val, y_val)
+    print("\n")
+
+# let's try to use the best model to predict the test data
+model_selected = input("Please select the model you want to use for prediction: ")
+df_test_values = pd.read_csv(f"{directory}test_values.csv")
+# converting the categorical variables into numerical values.
+for column in df_test_values.select_dtypes(include=['object']):
+    df_test_values[column],_ = pd.factorize(df_test_values[column])
+    # data normalization
+    min_max_scaler = MinMaxScaler()
+    x_train_scaled = min_max_scaler.fit_transform(df_train_values)
+    x_test_scaled = min_max_scaler.fit_transform(df_test_values)
+    # predict the test set
+    y_pred = model_dic[model_selected].predict(x_test_scaled)
 
